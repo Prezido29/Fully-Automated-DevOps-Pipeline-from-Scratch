56d26e13cce8b37d22d6c25341c72fe6bbe8e097
Sirecloud <mbavictor488@gmail,com>
2026-07-27 20:39:59 +0100

    add flask app helm chart

--- provider.tf
+++ provider.tf
@@ -4,9 +4,27 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "kubernetes" {
+  config_path = "C:/Users/prezido/Desktop/Devops/ec2-k8s-cluster/ec2-kubeconfig"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "C:/Users/prezido/Desktop/Devops/ec2-k8s-cluster/ec2-kubeconfig"
+  }
 }
