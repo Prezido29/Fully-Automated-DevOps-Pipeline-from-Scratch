33a519224cb3dd6c97385c75d56ff2d2376e6a7f
Sirecloud <mbavictor488@gmail,com>
2026-07-27 23:39:38 +0100

    updated python app code

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def get_current_time():
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     return f"The current time of the day: {current_time}"
 
+@app.route("/health")
+def health():
+    return "ok", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
